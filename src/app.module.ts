b9604ad1d31b321c1e5982f3cd24ab0dd77cec6b
Marcos Ricardo <mricardoalves99@gmail.com>
2023-03-17 18:02:42 -0300

    feat: add data-layers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {
+  BookController,
+  AuthorController,
+  GenreController,
+} from './controllers';
+import { DataServicesModule } from './services/data-services/data-services.module';
+import { BookUseCasesModule } from './use-cases/book/book-use-cases.module';
+import { AuthorUseCasesModule } from './use-cases/author/author-use-cases.module';
+import { GenreUseCasesModule } from './use-cases/genre/genre-use-cases.module';
+import { CrmServicesModule } from './services/crm-services/crm-services.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DataServicesModule,
+    BookUseCasesModule,
+    AuthorUseCasesModule,
+    GenreUseCasesModule,
+    CrmServicesModule,
+  ],
+  controllers: [BookController, AuthorController, GenreController],
+  providers: [],
 })
 export class AppModule {}
--- src/core/entities/book.entity.ts
+++ src/core/entities/book.entity.ts
@@ -1,6 +1,7 @@
 import { Author, Genre } from './';
 
 export class Book {
+
   title: string;
 
   author: Author;
